3ce697a28e18ce6398e9032057eb889d56e08134
Camt <tszhotang7@gmail.com>
2025-04-08 22:22:00 +0800

    1

--- c2n-contracts/hardhat.config.js
+++ c2n-contracts/hardhat.config.js
@@ -2,5 +2,36 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  networks: {
+    hardhat:{
+        chainId: 31337
+    },
+    local: {
+        url: 'http://127.0.0.1:8545',
+        chainId:31337,
+        // accounts: [process.env.LOCAL_PRIVATE_KEY]
+        accounts:['0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80']
+    },
+    sepolia: {
+        url: 'https://eth-sepolia.g.alchemy.com/v2/9wE1yRYtd9af7fc8KLTt-k_SuFJStzsS',
+        //accounts: [process.env.PRIVATE_KEY]
+        accounts:['fcbe63d30fadcbdc958152cb2afc99e3e29a647bb4edf20bcb2cebf045ec4d90']
+    }
+},
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+        details: {
+          yul: true,
+          yulDetails: {
+            stackAllocation: true
+          }
+        }
+      },
+      viaIR: true // Ensure viaIR is at the correct level
+    }
+  }
 };
